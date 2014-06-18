56a313c569ae2808cf855dd55508e392352e1f21
Cam Spiers <camspiers@gmail.com>
2014-06-18 16:43:45 +1200

    Add more test codes

--- tests/test.php
+++ tests/test.php
@@ -2,6 +2,26 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-var_dump(Flybuys\validate("6014 3590 0000 0928"));
-var_dump(Flybuys\validate("6014355065446212"));
-var_dump(Flybuys\validate("6014351000254605"));
+$codes = [
+    "6014359000000928",
+    "6014355065446212",
+    "6014351000254605",
+    "6014359000000019",
+    "6014359000000027",
+    "60143590000001128",
+    "6014359000000126",
+    "6014359000000217"
+];
+
+$passed = true;
+
+foreach ($codes as $code) {
+    if (Flybuys\validate($code)) {
+        echo 'Passed: ' . $code, PHP_EOL;
+    } else {
+        echo 'Failed: ' . $code, PHP_EOL;
+        $passed = false;
+    }
+}
+
+exit($passed ? 0 : 1);
